aaecc62dc1daf14f7ca8f663ae7aa181c3e59f8f
delfinapagano <dpagano@uade.edu.ar>
2024-04-10 11:34:33 -0300

    Primer version del footer + git ignore

--- tpo-api/src/pages/Home.tsx
+++ tpo-api/src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import NavBar from "@/components/ui/nav-bar";
+import Footer from "@/components/ui/footer";
 
 export default function () {
   return (
     <>
       <NavBar></NavBar>
+      <Footer/>
     </>
   );
 }
+
+
+
