e948d311498a5ece67d69124f2fc6547a6bd4263
Santiago Bianucci <111804118+SBianucci@users.noreply.github.com>
2024-05-03 16:24:54 -0300

    Merge - Login eye position fixed and product model changes (#6)

--- front/src/pages/LogIn.tsx
+++ front/src/pages/LogIn.tsx
@@ -1,6 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
+
 import image from "/src/assets/LogIn_Back.svg";
 import { useRef, useState } from "react";
 import { fetchUserByNameAndPassword } from "@/lib/users";
@@ -14,6 +14,12 @@ export default function LogIn() {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleLogin = async () => {
     if (userNameRef.current && passwordRef.current) {
@@ -60,40 +66,26 @@ export default function LogIn() {
             className="my-6"
             placeholder="Username"
           />
+          <div className="relative w-80 flex flex-col items-center justify-center mx-auto">
           <img
-            src="/src/assets/Eye.svg"
-            className="absolute right-0 top-0.25 transform -translate-y-1/2 mr-2 h-6 w-6"
-            alt="User Icon"
+              src={`/src/assets/${showPassword ? "eye-off.svg" : "eye.svg"}`}
+              onClick={togglePasswordVisibility}
+              className="absolute right-0 top-1/2 transform -translate-y-1/2 mr-2 h-6 w-6 cursor-pointer"
+              alt={showPassword ? "Hide Password" : "Show Password"}
           />
-          <Input ref={passwordRef} type="password" placeholder="Password" />
+          <Input 
+              ref={passwordRef} 
+              type={showPassword ? "text" : "password"}
+              placeholder="Password" />
+          </div>
+
           <Button
             onClick={handleLogin}
             className="w-80 mt-6 font-semibold text-lg bg-chiro-nopay"
           >
             Iniciar sesión
           </Button>
-          <h3 className="text-xs mt-2 mb-6 text-silk">
-            ¿Olvidaste tu contraseña?{" "}
-            <b className="font-semibold text-black">Recupérala</b>
-          </h3>
           {errorMessage ? errorMessage : null}
-        </div>
-        <div>
-          <Separator className="w-80 justify-center mx-auto bg-chiro-nopay" />
-          <div className=" flex justify-center space-x-4 mt-5">
-            <Button variant="outline" size="icon">
-              <img src="/src/assets/devicon_google.svg" alt="Google Icon"></img>
-            </Button>
-            <Button variant="outline" size="icon">
-              <img src="/src/assets/logos_apple.svg" alt="Apple Icon"></img>
-            </Button>
-            <Button variant="outline" className="" size="icon">
-              <img
-                src="/src/assets/logos_facebook.svg"
-                alt="Facebook Icon"
-              ></img>
-            </Button>
-          </div>
         </div>
       </div>
     </div>
